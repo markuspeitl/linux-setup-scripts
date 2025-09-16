6d35d0f6ad8745196c04773c3ec2149e66f3d12a
Markus Peitl <office@markuspeitl.com>
2025-09-16 18:56:40 +0200

    Change download/chroot script to wget and try fix

--- chroot_prod.sh
+++ chroot_prod.sh
@@ -3,11 +3,27 @@
 sudo apt update
 sudo apt install -y arch-install-scripts curl
 #wget -qO- devswarm.com:99/download_repo.sh | bash -s -- linux-setup-scripts
-curl -L devswarm.com:99/download_repo.sh | bash -s -- linux-setup-scripts
-cd /dev/shm/linux-setup-scripts || exit 1
 
-sudo python3 bootstrap_system_disk.py bootstrap.yml chroot --set prod_pc_setup -it
-#sudo apt install -y arch-install-scripts curl
+echo "Downloading pull script ..."
+
+wget -O /dev/shm/download_repo.sh devswarm.com:99/download_repo.sh
+
+bash /dev/shm/download_repo.sh linux-setup-scripts || exit 1
+#curl -L devswarm.com:99/download_repo.sh | bash -s -- linux-setup-scripts
+
+sleep 2
+cd /dev/shm/linux-setup-scripts/ || exit 1
+
+echo "Starting chroot mounting ..."
+
+sudo python3 bootstrap_system_disk.py bootstrap.yml chroot --set prod_pc_setup -it || exit 1
+
+if [ ! -d /tmp/bootschroot ]; then
+    echo "Chroot dir /tmp/bootschroot does not exist, exiting"
+    exit 1
+fi
+
+echo "Bind mounting and chrooting ..."
 
 sudo mkdir -p /tmp/bootschroot/scripts
 current_script_dir=$(dirname "$(realpath "$0")")
